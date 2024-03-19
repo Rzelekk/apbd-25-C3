1d1cee600351b56e690de15abf6c32ab0adc21b2
Rzelek <55500426+Rzelekk@users.noreply.github.com>
2024-03-19 11:31:27 +0100

    Add Kontener.cs

---
+++ ConsoleApp2/ConsoleApp2/Kontenery/Kontener.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp2.Exceptions;
+using ConsoleApp2.Interfaces;
+
+namespace ConsoleApp2.Kontenery;
+
+
+public class Kontener : IContainer
+{
+    public double MasaLadunku { get; set; }
+    public double MasaWlasna { get; set; }
+    public double Wysokosc { get; set; }
+    public double Glebokosc { get; set; }
+    
+
+
+    public Kontener(double masaLadunku, double wysokosc)
+    {
+        MasaLadunku = masaLadunku;
+        Wysokosc = wysokosc;
+    }
+    
+    
+    
+    public virtual void Unload()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Load(double cargoWeight)
+    {
+        throw new OverfillException();
+    }
+}
