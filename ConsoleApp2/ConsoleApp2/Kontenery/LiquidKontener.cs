f77c0ef6a045b73409abe9c42fe90d2fb929e198
Rzelek <55500426+Rzelekk@users.noreply.github.com>
2024-03-19 11:33:47 +0100

    Add LiquidKontener.cs

---
+++ ConsoleApp2/ConsoleApp2/Kontenery/LiquidKontener.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp2.Kontenery;
+
+public class LiquidKontener : Kontener
+{
+    public LiquidKontener(double masaLadunku, double wysokosc) : base(masaLadunku, wysokosc)
+    {
+        
+        
+        
+    }
+
+    public override void Unload()
+    {
+        base.Unload();
+    }
+}
