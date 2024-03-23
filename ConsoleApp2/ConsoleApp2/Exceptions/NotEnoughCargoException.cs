9e80cb7856a723f779d4aab24381a9329cfeef06
Rzelek <michalkop.t9l@gmail.com>
2024-03-23 09:31:01 +0100

    Add NotEnoughCargoException.cs

---
+++ ConsoleApp2/ConsoleApp2/Exceptions/NotEnoughCargoException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConsoleApp2.Exceptions;
+
+public class NotEnoughCargoException : Exception
+{
+    public NotEnoughCargoException()
+    {
+    }
+
+    protected NotEnoughCargoException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    public NotEnoughCargoException(string? message) : base(message)
+    {
+    }
+
+    public NotEnoughCargoException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
