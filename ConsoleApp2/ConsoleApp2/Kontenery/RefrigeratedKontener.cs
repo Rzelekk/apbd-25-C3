caa775bda32c0d032ca177286673c3dd108da2fa
Rzelek <michalkop.t9l@gmail.com>
2024-03-23 11:41:31 +0100

    add RefrigeratedKontener.cs and intefaces needed

---
+++ ConsoleApp2/ConsoleApp2/Kontenery/RefrigeratedKontener.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp2.Interfaces;
+
+namespace ConsoleApp2.Kontenery;
+
+public class RefrigeratedKontener : Kontener, IContainer
+{
+    public RefrigeratedKontener(string type, double masaWlasna, double wysokosc, double glebokosc, double maxMasaLadunku) : base(type, masaWlasna, wysokosc, glebokosc, maxMasaLadunku)
+    {
+    }
+
+    public override void Unload()
+    {
+        base.Unload();
+    }
+
+    public override void Load(double cargoWeight)
+    {
+        base.Load(cargoWeight);
+    }
+
+    public override void Unload(double weight)
+    {
+        base.Unload(weight);
+    }
+}
