7247c0a84292ed02350bab9f941064ec650c537c
Rzelek <michalkop.t9l@gmail.com>
2024-03-23 11:21:07 +0100

    Add load override implementation for LiquidKontener.cs

--- ConsoleApp2/ConsoleApp2/Kontenery/LiquidKontener.cs
+++ ConsoleApp2/ConsoleApp2/Kontenery/LiquidKontener.cs
@@ -12,6 +12,41 @@ public class LiquidKontener : Kontener, IHazardNotifier, IContainer
         
     }
 
+
+    public override void Unload(double weight)
+    {
+        base.Unload(weight);
+    }
+
+    public override void Load(double cargoWeight)
+    {
+        if (HazardCargo)
+        {
+            if (MasaLadunku + cargoWeight > MaxMasaLadunku / 2)
+            {
+                HazardNotification(NumerSeryjny,"Próba zaladowania ladunku niebezpiecznego w masie wiekszej niz dopuszczalne normy");
+               // throw new OverfillException("Masa ladunku przekracza dopuszczalny poziom dla ladunku niebezpiecznego!!");
+            }
+            else
+            {
+                MasaLadunku += cargoWeight;
+            }
+        }
+        else
+        {
+            if (MasaLadunku + cargoWeight > MaxMasaLadunku*0.9)
+            {
+                HazardNotification(NumerSeryjny,"Proba zaladowania kontenera ponad 90% dopuszczalnej ladownosci");
+                //throw new OverfillException("Chcesz zaladować więcej niż kontener może przyjąć");
+            }
+            else
+            {
+                MasaWlasna += cargoWeight;
+                Console.WriteLine("Zaladowano: "+cargoWeight+" kg, lączna masa ladunku: " + MasaLadunku +" kg");
+            }
+        }
+    }
+
     public override void Unload()
     {
         base.Unload();
