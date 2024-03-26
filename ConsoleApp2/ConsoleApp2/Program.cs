8fbf60727a9215170aa96cf60c352e13ecc7cc03
Rzelek <55500426+Rzelekk@users.noreply.github.com>
2024-03-26 01:21:30 +0100

    Add ShowMenu() method

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+void ShowMenu()
+{
+    Console.WriteLine("Możliwe akcje: ");
+    Console.WriteLine("1. Dodaj kontenerowiec");
+    Console.WriteLine("2. Dodaj kontener");
+    Console.WriteLine("3. Załaduj kontener");
+    Console.WriteLine("4. Wyładuj kontener");
+    Console.WriteLine("5. Załaduj jeden kontener na statek");
+    Console.WriteLine("6. Załaduj wszystkie kontenery na statek");
+    Console.WriteLine("7. Wyładuj jeden kontener ze statku");
+    Console.WriteLine("8. Wyładuj wszystkie kontenery ze statku");
+    Console.WriteLine("9. Zastąp kontener na statku innym");
+    Console.WriteLine("10. Przenieś kontener pomiędzy statkami");
+    Console.WriteLine("11. Pokaż informacje o kontenerze");
+    Console.WriteLine("12. Pokaż informacje o statku");
+    Console.WriteLine("13. Pokaż informacje o statku i jego ładunku");
+    Console.WriteLine("14. Wyjście");
+    
+    Console.WriteLine("\nWprowadz numer wybranej opcji: ");
+}
